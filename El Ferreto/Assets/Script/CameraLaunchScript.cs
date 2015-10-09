0f0083c2cc270b7a747a8da1b7502ff3ff20fdcd
Sam Potter <sam_pot_97@hotmail.com>
2015-10-09 12:34:53 +0100

    Basic Ferret functionality
    Testing ferret movement. The ferret can move left and right, as the
    device is tilted.
    The ferret can also fly, because why not?

---
+++ El Ferreto/Assets/Script/CameraLaunchScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLaunchScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Screen.autorotateToLandscapeLeft = false;
+        Screen.autorotateToLandscapeRight = false;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ El Ferreto/Assets/Script/movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+
+    const float speed = 20f;
+    const float speed_cap = 10f;
+
+    // Update is called once per frame
+    void Update () {
+        Vector2 movement = new Vector2(
+            Input.acceleration.x,
+            Input.acceleration.y
+            );
+
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+        body.velocity += (movement * Time.deltaTime * speed);
+        body.freezeRotation = true;
+        
+    }
+}
