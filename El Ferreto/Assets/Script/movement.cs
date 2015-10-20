41683841715830c8f0e4317b75750a4f5e097ffd
Sam Potter <sam_pot_97@hotmail.com>
2015-10-20 11:39:09 +0100

    Fixed Commit reversion
    Some previous changes were removed somehow, so they have been re-added.

--- El Ferreto/Assets/Script/movement.cs
+++ El Ferreto/Assets/Script/movement.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using System;
 
-public class movement : MonoBehaviour {
+public class movement : MonoBehaviour
+{
 
     private Rigidbody2D body;
-	private Animator anim;
+    private Animator anim;
     //Stores last y accelerometer states where 0 is the current state
     private float[] jump_state = new float[10];
 
@@ -21,10 +22,10 @@ public class movement : MonoBehaviour {
     {
         GetComponent<Rigidbody2D>().freezeRotation = true;
         body = GetComponent<Rigidbody2D>();
-		anim = gameObject.GetComponent<Animator> ();
+        anim = gameObject.GetComponent<Animator>();
     }
 
-    void Update () 
+    void Update()
     {
         Rigidbody2D body = GetComponent<Rigidbody2D>();
         Vector2 movement;
@@ -38,79 +39,59 @@ public class movement : MonoBehaviour {
             movement = new Vector2(Input.acceleration.x, 0);
             pollJumpStates();
         }
-        
+
         //Jump, if the player has tried to jump and is touching the ground
-        if (hasJumped() && touching_ground) 
+        if (hasJumped() && touching_ground)
         {
             jump(body);
         }
 
         body.velocity += (movement * Time.deltaTime * speed);
 
-		// setting ground condition for Animator
-		anim.SetBool ("ground", touching_ground);
+        animationUpdate();
+    }
 
-		// setting speed condition for Animator
-		if (keyboard_controlled) {
-			anim.SetFloat ("speed", Mathf.Abs (Input.GetAxis ("Horizontal")));
-		} 
-		else 
-		{
-			anim.SetFloat ("speed", Input.acceleration.x);
-		}
+    void animationUpdate()
+    {
+        // setting ground condition for Animator
+        anim.SetBool("ground", touching_ground || Math.Round(body.velocity.x * 100) == 0);
 
-		// sprite faces right if moving forward
-		if (keyboard_controlled) 
-		{
-			if (Input.GetAxis ("Horizontal") > 0.1f) {
-				transform.localScale = new Vector3 (1, 1, 1);
-			}
-		} 
-		else 
-		{
-			if(Input.acceleration.x > 0.1f)
-			{
-				transform.localScale = new Vector3 (1, 1, 1);
-			}
-		}
-		// sprite faces left if moving back
-		if (keyboard_controlled) 
-		{
-			if (Input.GetAxis ("Horizontal") > 0.1f) {
-				transform.localScale = new Vector3 (1, 1, 1);
-			}
-		} 
-		else 
-		{
-			if(Input.acceleration.x < -0.1f)
-			{
-				transform.localScale = new Vector3 (-1, 1, 1);
-			}
-		}
+        // setting speed condition for Animator
+        anim.SetFloat("speed", Mathf.Abs(body.velocity.x));
+
+        //Flip sprite to face correct direction
+        if (body.velocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (body.velocity.x > 0f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
     //Shift every value in the array and add current state
-    void pollJumpStates() 
+    void pollJumpStates()
     {
         float current_state = Input.acceleration.y;
 
-        for (int i = jump_state.Length-1; i > 0; i--) 
+        for (int i = jump_state.Length - 1; i > 0; i--)
         {
-            jump_state[i] = jump_state[i-1];
+            jump_state[i] = jump_state[i - 1];
         }
 
         jump_state[0] = current_state;
     }
 
-    void jump(Rigidbody2D body) 
+    void jump(Rigidbody2D body)
     {
         //If the player is on the ground and they jump
-        body.velocity+= new Vector2(0f, jump_height);
+        body.velocity += new Vector2(0f, jump_height);
         touching_ground = false;
     }
 
     //Has the player tried to jump
-    bool hasJumped() 
+    bool hasJumped()
     {
         if (keyboard_controlled) return Input.GetKeyDown(KeyCode.W);
 
@@ -118,29 +99,30 @@ public class movement : MonoBehaviour {
         {
             return true;
         }
-        else 
+        else
         {
             return false;
         }
     }
 
-    float getJumpMagnitude() 
+    float getJumpMagnitude()
     {
-        return Math.Abs(jump_state[0] - jump_state[jump_state.Length-1]);    
+        return Math.Abs(jump_state[0] - jump_state[jump_state.Length - 1]);
     }
 
-    void OnCollisionEnter2D(Collision2D collision_object) 
+    void OnCollisionEnter2D(Collision2D collision_object)
     {
         //If the player is touching buildings, it will be classed as on the ground
-        if (collision_object.gameObject.tag == "ground") {
+        if (collision_object.gameObject.tag == "ground")
+        {
             touching_ground = true;
 
         }
     }
 
     //For debugging purposes only
-    void OnGUI() 
+    void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 1000, 100), "Debug:\n" + body.position + "\n" + Input.acceleration + "\n" + (int)(getJumpMagnitude()*100)/100f);
+        GUI.Label(new Rect(100, 10, 1000, 100), "Debug:\n" + body.position + "\n" + Input.acceleration + "\n" + (int)(getJumpMagnitude() * 100) / 100f);
     }
 }
