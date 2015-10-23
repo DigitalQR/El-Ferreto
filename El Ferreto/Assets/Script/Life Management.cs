87620a3905be136397f8f98aa92bf909bf4979cc
shinigamirei <shinigamireyas@googlemail.com>
2015-10-23 10:32:03 +0100

    health system

---
+++ El Ferreto/Assets/Script/Life Management.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeManagement : MonoBehaviour
+{
+    public int Health;
+    public int gameover;
+
+    void Start()
+    {
+        Health = 3;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Health = Health - 1;
+        }
+        else if (other.gameObject.tag == "Offscreen")
+        {  
+            Health = 0;
+        }
+        
+    }
+}
