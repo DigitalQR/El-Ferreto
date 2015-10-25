2e035b65e13a2413e0cc4d47fdbc5526c950b116
Cristian Bujor <cristi.bujor@outlook.com>
2015-10-25 19:50:59 +0000

    Added Menu
    A pause button must be added as the pause menu works through the back
    arrow at the moment

---
+++ El Ferreto/Assets/Script/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	//Variable for enabling/disabling pause menu
+	public GameObject GameMenu2;
+
+	private bool paused = false;
+	
+	void Start(){
+	
+		GameMenu2.SetActive (true);
+		Time.timeScale = 0;
+
+	}
+
+	public void Resume(){
+		
+		paused = false;
+		
+	}
+
+	public void Restart(){
+
+		GameMenu2.SetActive (false);
+		Application.LoadLevel(1); //Game loads the first scene
+
+	}
+
+	public void Exit(){
+
+		Application.Quit ();
+
+	}
+}
