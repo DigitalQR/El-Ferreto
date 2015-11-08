d1a903cb1374b853bc14785fdbdbfaa1e2b1532e
Sam Potter <sam_pot_97@hotmail.com>
2015-11-08 15:09:13 +0000

    Off-screen Camera toggle
    Small camera toggles, if the player is off screen.

---
+++ El Ferreto/Assets/Script/Level/OffScreenCameraToggle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffScreenCameraToggle : MonoBehaviour {
+
+    public Camera camera;
+
+    void OnTriggerEnter2D(Collider2D collision_object)
+    {
+        if (collision_object.gameObject.name == "ferret")
+        {
+            camera.enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision_object)
+    {
+        if (collision_object.gameObject.name == "ferret")
+        {
+            camera.enabled = false;
+        }
+
+    }
+
+}
