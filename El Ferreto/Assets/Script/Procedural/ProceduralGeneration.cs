9629b4b4954cafe9d44be7ed49f9ba48c68b7ee7
JonatanBiegalski <therealyonish@gmail.com>
2015-11-09 16:41:32 +0000

    Procedural Generation 1
    First Setup, not full code

---
+++ El Ferreto/Assets/Script/Procedural/ProceduralGeneration.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralGeneration : MonoBehaviour {
+	
+	public static GameObject[] myObjects;
+
+	void Start () 
+	{
+		myObjects = Resources.LoadAll<GameObject>("Prefabs");
+	}
+
+	void SpawnRandomBuilding()
+	{
+		int number = Random.Range (0, 10);
+		GameObject myObj = Instantiate(myObjects[number],
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnBecomeInvisible()
+	{
+		Destroy (Prefab);
+		SpawnRandomBuilding();
+	}
+}
