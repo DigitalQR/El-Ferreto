7b28374347882e7e311994753499f96c8498a2bd
Sam Potter <sam_pot_97@hotmail.com>
2015-11-24 22:45:55 +0000

    Item button
    Item button on shop front.

---
+++ El Ferreto/Assets/Script/Shop/ShopButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShopButton : MonoBehaviour {
+
+    private static uint item_track = 0;
+    private uint ID = item_track++;
+
+    public Text header;
+    public Text price;
+    public Button button;
+    public GameObject item;
+
+    void Start()
+    {
+        updateDisplay();
+        Debug.Log("Item '" + item.name + "' ID:" + ID);
+    }
+
+    public void updateDisplay()
+    {
+        GameObject display_item = Instantiate(item);
+
+        header.text = item.GetComponent<Item>().item_name;
+        price.text = "" + item.GetComponent<Item>().item_price;
+
+        display_item.transform.position = new Vector3(transform.position.x-0.3f, transform.position.y - 0.4f, -1);
+        display_item.transform.SetParent(transform);
+    }
+
+}
--- El Ferreto/Assets/Script/Shop/ShopManager.cs
+++ El Ferreto/Assets/Script/Shop/ShopManager.cs
@@ -3,11 +3,13 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ShopManager : MonoBehaviour {
-
+    
     public Text currency;
-
-    void Update() {
+    
+    void Update()
+    {
         if (!PlayerPrefs.HasKey("Currency")) PlayerPrefs.SetInt("Currency", 0);
         currency.text = "" + PlayerPrefs.GetInt("Currency");
+        
     }
 }
