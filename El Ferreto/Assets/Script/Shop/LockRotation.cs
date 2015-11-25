25f323d7dce30ff57c37a5e6df7eb7ff8249faf5
Sam Potter <sam_pot_97@hotmail.com>
2015-11-25 13:35:38 +0000

    Added trail
    Added trails and movement in store.

---
+++ El Ferreto/Assets/Script/Shop/LockRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockRotation : MonoBehaviour {
+
+    void Update()
+    {
+        if (transform.lossyScale.x == -1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (transform.lossyScale.x == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }
+	
+}
--- El Ferreto/Assets/Script/Shop/ShopButton.cs
+++ El Ferreto/Assets/Script/Shop/ShopButton.cs
@@ -4,27 +4,47 @@ using System.Collections;
 
 public class ShopButton : MonoBehaviour {
 
+    private float track = 0;
+    private float range = 0.01f;
+
     public GameObject item;
     public bool hat;
+    public bool move_object = false;
     public Text header;
     public Text price;
     public Button button;
     public GameObject model;
 
+    public GameObject display_item;
+
     void Start()
     {
-        updateDisplay();
+        if (item != null) updateDisplay();
+        else 
+        {
+            header.text = "Nothing";
+            price.text = "";
+        }
     }
 
     public void updateDisplay()
     {
-        GameObject display_item = Instantiate(item);
+        display_item = Instantiate(item);
 
-        header.text = item.GetComponent<Item>().item_name;
+        header.text = item.GetComponent<Item>().item_name; 
         price.text = "" + item.GetComponent<Item>().item_price;
 
         display_item.transform.position += new Vector3(model.transform.position.x, model.transform.position.y, 1);
         display_item.transform.SetParent(model.transform);
+    }
+
+    void Update()
+    {
+        if (display_item != null && move_object)
+        {
+            track += Time.deltaTime;
+            display_item.transform.Translate(new Vector3(Mathf.Cos(track), 0, 0) * range);
+        }
     }
 
     public void OnClick()
