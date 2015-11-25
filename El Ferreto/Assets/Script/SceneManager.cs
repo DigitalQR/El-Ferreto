b027c2bf1f9c2be4de7f4d45d70038434d3ce672
Sam Potter <sam_pot_97@hotmail.com>
2015-11-25 00:29:04 +0000

    Debug button to enter game

---
+++ El Ferreto/Assets/Script/SceneManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+    public void switchScene(string scene) {
+        Application.LoadLevel(scene);
+    }
+}
