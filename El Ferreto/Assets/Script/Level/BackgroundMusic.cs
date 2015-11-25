b67d5c6645a6e88bed55258c9778962e87912fdd
centinil <lorenzo@centini.info>
2015-11-25 12:53:59 +0000

    Fixed the Jumping Audio
    The jumping audio was not working now fixed. Added audio sound for
    background music

---
+++ El Ferreto/Assets/Script/Level/BackgroundMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    public AudioClip background_music;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+	}
+}
