183506551bc8923a5efe403592acb39f202d7bae
Sam Potter <sam_pot_97@hotmail.com>
2015-11-25 14:18:13 +0000

    Orbiting RGB effect
    RGB that orbits the player.

---
+++ El Ferreto/Assets/Script/Shop/ConstantRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour {
+
+    public float angular_speed = 0.1f;
+    public float distance;
+    public float angle;
+
+    void Start()
+    {
+        angle = Mathf.Rad2Deg * angle;
+    }
+
+	void Update () {
+        angle += angular_speed * Time.deltaTime;
+        transform.localPosition = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0)*distance;
+	}
+}
