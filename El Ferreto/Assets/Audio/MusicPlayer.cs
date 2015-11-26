c6d753045e035192d9099fa607de1fd56d713074
centinil <lorenzo@centini.info>
2015-11-26 14:05:37 +0000

    Added Random Background Music Player
    Music randomly plays from the two different audio clips. (There is a bug
    that one music clip will restart every time the ferret jumps)

---
+++ El Ferreto/Assets/Audio/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public AudioClip[] clips;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = FindObjectOfType<AudioSource>();
+        audioSource.loop = false;
+	}
+	
+    private AudioClip GetRandomClip()
+        {
+	        return clips[Random.Range(0, clips.Length)]; //looking for audio clips from 0 to n of clips
+        }
+
+
+	// Update is called once per frame
+	void Update () {
+	    if (!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        
+        }
+	}
+}
