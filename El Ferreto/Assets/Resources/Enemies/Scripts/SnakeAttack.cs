fd54ffe5c02289d25c96034cf9d225ab6713fe66
abujabez <abujaber.zaid@gmail.com>
2015-11-29 14:46:04 +0000

    Added snake enemy
    Snake fires fireballs in a set interval.
    Doesn't damage, currently.

---
+++ El Ferreto/Assets/Resources/Enemies/Scripts/SnakeAttack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeAttack : MonoBehaviour
+{
+    [Range (0,1)]
+    public float fire_time;
+    private float current_state = 0;
+    private int current_degree = 0;
+    private bool can_fire = true;
+
+    public GameObject fireball;
+
+    void Update()
+    {
+        AnimatorStateInfo animation = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+        current_state = animation.normalizedTime - current_degree;
+
+        while (current_state >= 1)
+        {
+            current_degree++;
+            current_state -= 1;
+            can_fire = true;
+        }
+
+        if (fire_time >= current_state && can_fire)
+        {
+            can_fire = false;
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject spitball = Instantiate(fireball);
+        spitball.transform.position = transform.position;
+    }
+}
---
+++ El Ferreto/Assets/Resources/Enemies/Scripts/SpitballScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpitballScript : MonoBehaviour {
+
+    public float speed = 0.1f;
+    private Rigidbody2D body;
+
+	void Start ()
+    {
+        GameObject ferret = GameObject.Find("ferret");
+        body = GetComponent<Rigidbody2D>();
+
+        Vector2 direction = new Vector2(
+            ferret.transform.position.x - transform.position.x,
+            ferret.transform.position.y - transform.position.y          
+           
+        );
+
+        body.velocity = direction.normalized * speed;
+
+
+        body.rotation = Vector2.Angle(-Vector2.right, direction);
+
+        if (direction.y > 0)
+        {
+            body.rotation *= -1;
+        }
+    }
+}
